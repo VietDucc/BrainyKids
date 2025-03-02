bd9d1350cc2fc58017cd27a87ef1012e26c942cc
VietDucc <21521959@gm.uit.edu.vn>
2025-03-02 10:39:41 +0700

    add deloy.yml

--- src/main/java/com/example/demo/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/demo/dto/request/UserCreationRequest.java
@@ -2,14 +2,16 @@ package com.example.demo.dto.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 // DTO (Data Transfer Object) là một class đơn giản chứa các thuộc tính và phương thức getter/setter để truyền dữ liệu giữa các lớp.
 public class UserCreationRequest {
 
+    @NotBlank(message = "Tên không dược để trống")// Kiem tra khong duoc de trong
     private String name;
 
-    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Size(min = 8, message = "Mật khẩu phải dài hơn 8 ký tự")// Vi dong nay nam tren bien password nen spring biet rang rang buoc nay duoc ap dung cho truong password
     private String password;
     private String firstName;
     private String lastName;
--- src/main/java/com/example/demo/exeption/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exeption/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.example.demo.exeption;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //Noi su li tap trung cac exception
 @ControllerAdvice
@@ -11,4 +15,17 @@ public class GlobalExceptionHandler {
     ResponseEntity<String> handlingRuntimeException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<Map<String, String>> handlingValidation(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Lặp qua tất cả các lỗi và thêm vào map
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 }
