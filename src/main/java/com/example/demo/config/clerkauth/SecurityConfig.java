d70b21adfdc7fb98db4f6bd1916e5ef1713c8a0e
VietDuc <21521959@gm.uit.edu.vn>
2025-04-03 09:31:27 +0700

    Vietduc/jwt authen (#9)
    * vietduc:jwt authen
    
    * Update .yaml

--- src/main/java/com/example/demo/config/clerkauth/SecurityConfig.java
+++ src/main/java/com/example/demo/config/clerkauth/SecurityConfig.java
@@ -1,32 +1,43 @@
-//package com.example.demo.config.clerkauth;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Autowired
-//    private ClerkAuthFilter clerkAuthFilter;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(
-//                                "/v3/api-docs/**",  // API docs
-//                                "/swagger-ui/**",   // Swagger UI
-//                                "/swagger-ui.html"  // Swagger main page
-//                        ).permitAll()  // Mở quyền truy cập
-//                        .anyRequest().authenticated()  // Còn lại cần xác thực
-//                )
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .addFilterBefore(clerkAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
-//}
+package com.example.demo.config.clerkauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/users/clerk").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -4,18 +4,37 @@ import com.example.demo.dto.request.CourseRequest;
 import com.example.demo.entity.Course;
 import com.example.demo.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CourseService {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private static final String REDIS_COURSES_KEY = "all_courses"; // Key Redis
 
     @Autowired
     private CourseRepository courseRepository;
 
     public List<Course> getAllCourses() {
-        return courseRepository.findAll();
+
+//        return courseRepository.findAll();
+        // Kiểm tra xem danh sách Course có trong Redis không
+        List<Course> courses = (List<Course>) redisTemplate.opsForValue().get(REDIS_COURSES_KEY);
+
+        if (courses == null) {
+            // Nếu không có trong Redis, lấy từ Database
+            courses = courseRepository.findAll();
+
+            // Lưu vào Redis với thời gian 30 phút
+            redisTemplate.opsForValue().set(REDIS_COURSES_KEY, courses, 30, TimeUnit.MINUTES);
+        }
+
+        return courses;
     }
 
     public Optional<Course> getCourseById(Long id) {
