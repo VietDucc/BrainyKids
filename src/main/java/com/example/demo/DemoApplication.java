a9493bedaa738bf203e3ff19edcbe9a0231fc7cb
VietDucc <21521959@gm.uit.edu.vn>
2025-05-29 00:28:26 +0700

    Vnpay create

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableConfigurationProperties
 public class DemoApplication {
 
 	public static void main(String[] args) {
