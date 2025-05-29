26ee86ffe5f9363b9d399b73bf41bd91562a676a
VietDuc <21521959@gm.uit.edu.vn>
2025-05-29 11:22:55 +0700

    Nguyenkhoa/new feature (#38)
    * add Blog + blog comments
    
    * add imgSrc to challenge
    
    * add Lesson difficulties
    
    * add Vocabulary in Lesson(vocab-quiz)
    
    * remove flashcards from getDeck, only show in details
    added PUT for user-deck
    /bulk for POST an array of flashcards in user-flashcard-controller
    blog now also returns authorName and authorImg
    comments now also returns authorName and authorImg
    
    * add user/admin check to all public feature
    check GET UserDeck through deckId
    accepting excel file for vocabulary POST
    accepting array of JSON object for vocabulary POST
    fix PUT for userDeck
    accepting array of JSON object for flashcard
    
    ---------
    
    Co-authored-by: donghuunguyenkhoa <donghuunguyenkhoa@gmail.com>

--- src/main/java/com/example/demo/controller/UnitController.java
+++ src/main/java/com/example/demo/controller/UnitController.java
@@ -5,6 +5,7 @@ import com.example.demo.dto.response.UnitResponse;
 import com.example.demo.entity.Unit;
 import com.example.demo.service.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,16 +21,19 @@ public class UnitController {
         return unitService.getUnitsByCourseId(courseId);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/{courseId}")
     public Unit createUnit(@PathVariable Long courseId, @RequestBody UnitRequest unitRequest) {
         return unitService.createUnit(courseId, unitRequest);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{unitId}")
     public Unit updateUnit(@PathVariable Long unitId, @RequestBody UnitRequest unitRequest) {
         return unitService.updateUnit(unitId, unitRequest);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{unitId}")
     public void deleteUnit(@PathVariable Long unitId) {
         unitService.deleteUnit(unitId);
