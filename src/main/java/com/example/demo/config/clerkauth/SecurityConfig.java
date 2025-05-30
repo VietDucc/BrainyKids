8f89767b4446492c28265a27f70d84bef800955c
VietDucc <21521959@gm.uit.edu.vn>
2025-05-30 12:56:34 +0700

    change author IPN

--- src/main/java/com/example/demo/config/clerkauth/SecurityConfig.java
+++ src/main/java/com/example/demo/config/clerkauth/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/users/clerk").permitAll()
-                        .requestMatchers("/demo/IPN").permitAll()
+                        .requestMatchers("/IPN").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
